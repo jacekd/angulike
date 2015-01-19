a003282a72886b6137076764bd4c1029e3d1b673
Jacek Dominiak <jacek@jmbp.local>
2015-01-19 21:38:15 +0100

    can't get those FB buttons to work, @marc, can you check?

--- angulike.js
+++ angulike.js
@@ -46,7 +46,7 @@
                                 return;
                             } else {
                                 element.html('<div class="fb-like"' + (!!scope.fbLike ? ' data-href="' + scope.fbLike + '"' : '') + ' data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>');
-                                $window.FB.XFBML.parse(element.parent()[0]);
+                                $window.FB.XFBML.parse(element[0].parent);
                             }
                         }
                     }
@@ -59,17 +59,36 @@
                 return {
                     restrict: 'A',
                     link: function (scope, element, attrs) {
-                    element.html('<div id="fb-root"></div><script>(function(d, s, id) { ' +
-                        ' var js, fjs = d.getElementsByTagName(s)[0]; ' +
-                            ' if (d.getElementById(id)) return; ' +
-                            ' js = d.createElement(s); js.id = id; ' +
-                            ' js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0"; ' +
-                            ' fjs.parentNode.insertBefore(js, fjs);' +
-                            '}(document, "script", "facebook-jssdk"));</script>' +
-                        '<div class="fb-share-button" ' +
-                        ' data-href="' + (!!attrs.href ? attrs.href : $window.location.href) + '"' +
-                        ' data-layout="' + (!!attrs.layout ? attrs.layout : 'button') + '"' +
-                        '></div>');
+                        if (!$window.FB) {
+                            // Load Facebook SDK if not already loaded
+                            $.getScript('//connect.facebook.net/en_US/sdk.js', function () {
+                                renderShareButton();
+                            });
+                        } else {
+                            renderShareButton();
+                        }
+                        var watchAdded = false;
+
+                        function renderShareButton() {
+                            if (!!attrs.fbLike && !watchAdded) {
+                                // wait for data if it hasn't loaded yet
+                                var watchAdded = true;
+                                return;
+                            } else {
+                                element.html('<div id="fb-root"></div><script>(function(d, s, id) { ' +
+                                ' var js, fjs = d.getElementsByTagName(s)[0]; ' +
+                                ' if (d.getElementById(id)) return; ' +
+                                ' js = d.createElement(s); js.id = id; ' +
+                                ' js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0"; ' +
+                                ' fjs.parentNode.insertBefore(js, fjs);' +
+                                '}(document, "script", "facebook-jssdk"));</script>' +
+                                '<div class="fb-share-button" ' +
+                                ' data-href="' + (!!attrs.href ? attrs.href : $window.location.href) + '"' +
+                                ' data-layout="' + (!!attrs.layout ? attrs.layout : 'button') + '"' +
+                                '></div>');
+                                //$window.FB.XFBML.parse(element[0].parent);
+                            }
+                        }
                     }
                 };
             }
@@ -102,9 +121,6 @@
             '$window', function ($window) {
                 return {
                     restrict: 'A',
-                    scope: {
-                        tweet: '='
-                    },
                     link: function (scope, element, attrs) {
                         if (!$window.twttr) {
                             // Load Twitter SDK if not already loaded
@@ -115,36 +131,17 @@
                             renderTweetButton();
                         }
 
-                        var watchAdded = false;
 
                         function renderTweetButton() {
-                            // fallback for no scope
-                            if (!!attrs.text) {
-                                scope.tweet = attrs.text;
-                            }
-                            if (!scope.tweet && !watchAdded) {
-                                // wait for data if it hasn't loaded yet
-                                watchAdded = true;
-                                var unbindWatch = scope.$watch('tweet', function (newValue, oldValue) {
-                                    if (newValue) {
-                                        renderTweetButton();
-
-                                        // only need to run once
-                                        unbindWatch();
-                                    }
-                                });
-                                return;
-                            } else {
-                                element.html('<a href="https://twitter.com/share" class="twitter-share-button" ' +
-                                'data-text="' + scope.tweet + '"' +
-                                ' data-url="' + (!!attrs.url ? attrs.url : $window.location.href) + '"' +
-                                (!!attrs.count ? ' data-count="' + attrs.count + '"' : '') +
-                                (!!attrs.via ? ' data-via="' + attrs.via + '"' : '') +
-                                (!!attrs.related ? ' data-related="' + attrs.related + '"' : '') +
-                                (!!attrs.hashtags ? ' data-hashtags="' + attrs.hashtags + '"' : '') +
-                                '">Tweet</a>');
-                                $window.twttr.widgets.load(element.parent()[0]);
-                            }
+                            element.html('<a href="https://twitter.com/share" class="twitter-share-button" ' +
+                            'data-text="' + attrs.text + '"' +
+                            ' data-url="' + (!!attrs.url ? attrs.url : $window.location.href) + '"' +
+                            (!!attrs.count ? ' data-count="' + attrs.count + '"' : '') +
+                            (!!attrs.via ? ' data-via="' + attrs.via + '"' : '') +
+                            (!!attrs.related ? ' data-related="' + attrs.related + '"' : '') +
+                            (!!attrs.hashtags ? ' data-hashtags="' + attrs.hashtags + '"' : '') +
+                            '">Tweet</a>');
+                            $window.twttr.widgets.load(element.parent()[0]);
                         }
                     }
                 };
@@ -227,7 +224,7 @@
                             + (!!attrs.counter ? 'data-counter="' + attrs.counter + '"' : '') +
                             + (!!attrs.url ? 'data-url="' + attrs.url + '"' : '') +
                             '></script>');
-                            $window.IN.init();
+                            $window.IN.parse();
                         }
                     }
                 }
